fffe0c03a9abd59fd2c6127e0ecaca2a6601b175
Adisa Narula <adisan19@gmail.com>
2022-01-23 16:49:12 +0700

    signed and verify sender

--- client/index.js
+++ client/index.js
@@ -1,8 +1,33 @@
 import "./index.scss";
 
+
+/**
+ * Key class used to store private key, sender pair from the server
+ */
+class PrivateKey {
+  constructor() {
+    this.sender = "";
+    this.private = "";
+  }
+
+  setSender(sender) {
+    this.sender = sender;
+  }
+
+  setPrivateKey(key) {
+    this.private = key;
+  }
+
+  getKeyFromSender(sender) {
+    if (sender === this.sender) return this.private;
+  }
+}
+
 const server = "http://localhost:3042";
+const key = new PrivateKey();
 
 document.getElementById("exchange-address").addEventListener('input', ({ target: {value} }) => {
+  
   if(value === "") {
     document.getElementById("balance").innerHTML = 0;
     return;
@@ -10,7 +35,17 @@ document.getElementById("exchange-address").addEventListener('input', ({ target:
 
   fetch(`${server}/balance/${value}`).then((response) => {
     return response.json();
-  }).then(({ balance }) => {
+  }).then((res) => {
+
+    const {balance, privateKey} = res;
+    if (!balance) {
+      document.getElementById("balance").innerHTML = 0;
+      return;
+    }
+
+    key.setSender(value);
+    key.setPrivateKey(privateKey);
+
     document.getElementById("balance").innerHTML = balance;
   });
 });
@@ -19,9 +54,11 @@ document.getElementById("transfer-amount").addEventListener('click', () => {
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  const privateKey = key.getKeyFromSender(sender);
+
 
   const body = JSON.stringify({
-    sender, amount, recipient
+    sender, amount, recipient, privateKey
   });
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
@@ -30,5 +67,7 @@ document.getElementById("transfer-amount").addEventListener('click', () => {
     return response.json();
   }).then(({ balance }) => {
     document.getElementById("balance").innerHTML = balance;
-  });
+  }).catch(error => {
+    console.log(error);
+  }) 
 });
--- server/index.js
+++ server/index.js
@@ -2,11 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3042;
-
-// keys & hashing
-const SHA256 = require('crypto-js/sha256');
-const EC = require('elliptic').ec;
-const ec = new EC('secp256k1');
+const helper = require('./helper');
 
 
 // localhost can have cross origin errors
@@ -14,40 +10,42 @@ const ec = new EC('secp256k1');
 app.use(cors());
 app.use(express.json());
 
-const balances = [];
+const balances = {};
 
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
-  const balance = balances[address] || 0;
-  res.send({ balance });
+  const {balance, privateKey} = balances[address] || 0;
+
+  res.send({ balance, privateKey });
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+  const {sender, recipient, amount, privateKey} = req.body;
+
+  if (!balances[sender]) {
+    res.status(500).send({ error: "Sender does not exist"});
+    return;
+  }
+
+  const {x, y} = balances[sender];
+  const msg = {sender, recipient, amount};
+
+  // sign message with private key
+  const {message, signature} = helper.signMessage(privateKey, msg);
+  const verify = helper.verifyMessage(signature, {x, y}, message);
+
+  if (verify) {
+    console.log('Transfering funds...');
+    balances[sender].balance -= amount;
+    balances[recipient].balance = (balances[recipient].balance || 0) + +amount;
+    res.send({ balance: balances[sender].balance });
+  } else {
+    res.status(500).send({ err: "Could not verify sender"});
+  }
+
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
-
-  for (let i = 0; i < 3; i++) {
-
-    // get keys
-    const keyPair = ec.genKeyPair();
-    const public = keyPair.getPublic().encode('hex');
-    const private = keyPair.getPrivate().toString();
-
-    balances.push({
-      balance: 1000,
-      public,
-      private
-    });
-  }
-
-  // print out balances
-  for (let key in balances) {
-    console.log(balances[key]);
-  }
+  helper.generateKeysAndBalances(balances, process.env.accounts);
 });
