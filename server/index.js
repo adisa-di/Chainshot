bc3d466565c6bea616e27921ea704e98e3395757
Adisa Narula <adisan19@gmail.com>
2022-01-23 14:53:56 +0700

    part 1

--- server/index.js
+++ server/index.js
@@ -3,16 +3,18 @@ const app = express();
 const cors = require('cors');
 const port = 3042;
 
+// keys & hashing
+const SHA256 = require('crypto-js/sha256');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
-}
+const balances = [];
 
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
@@ -29,4 +31,23 @@ app.post('/send', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
+
+  for (let i = 0; i < 3; i++) {
+
+    // get keys
+    const keyPair = ec.genKeyPair();
+    const public = keyPair.getPublic().encode('hex');
+    const private = keyPair.getPrivate().toString();
+
+    balances.push({
+      balance: 1000,
+      public,
+      private
+    });
+  }
+
+  // print out balances
+  for (let key in balances) {
+    console.log(balances[key]);
+  }
 });
